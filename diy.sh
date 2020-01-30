5a60ddd4f27f9252a1be14555f4b2a8cffe4d6c7
dd-gjk <60371627+dd-gjk@users.noreply.github.com>
2020-01-31 06:37:01 +0800

    Update diy.sh
    添加无线驱动

--- diy.sh
+++ diy.sh
@@ -7,3 +7,4 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/MeIsReallyBa/k2p-openwrt-mt7615_5.0.2.0 package/mtk
