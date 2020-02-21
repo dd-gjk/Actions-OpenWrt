c81d4224da09bacf7e4cb0e949bb4033c48031ba
dd-gjk <60371627+dd-gjk@users.noreply.github.com>
2020-02-22 07:28:39 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,4 +8,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #git clone https://github.com/MeIsReallyBa/k2p-openwrt-mt7615_5.0.2.0 package/mtk
-git clone https://github.com/pymumu/luci-app-smartdns package/lean
+git clone https://github.com/pymumu/luci-app-smartdns package/lean/luci-app-smartdns
